7189df63cbe0da3d9ca18ff5724abfbd91ff655d
RaicuMarian <raicumarian35@gmail.com>
2022-01-12 13:27:01 +0200

    Adaugare sectiune "certificari", Adaugare link profil LinkedIn

--- app.js
+++ app.js
@@ -17,5 +17,39 @@ $( document ).ready(function(){
     );
   });
 
+
+  // Galerie certificari
+
+  const overlay = document.querySelector('.overlay');
+  const overlayInner = document.querySelector('.overlay-inner');
+  const overlayImage = overlay.querySelector('img');
+  const overlayClose = overlay.querySelector('.close');
+  
+
+  function handleClick(e) {
+    const src = e.currentTarget.querySelector('img').src;
+    overlayImage.src = src;
+    overlay.classList.add('open');
+  }
+
+  
+  overlay.addEventListener('click',function(e){
+    // console.log(e.target);
+    if (e.target != overlayImage){
+      overlay.classList.remove('open')
+    }
+  })
+
+  function close() {
+    overlay.classList.remove('open');
+  }
+
+  const items = document.querySelectorAll('.itemGallery');
+
+  items.forEach(item => item.addEventListener('click', handleClick));
+
+  overlayClose.addEventListener('click', close);
+ 
+
     
 
